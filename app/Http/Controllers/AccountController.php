8417dae1ffb9c40f00a42d4efbe98aa1e1550ef0
ayyanovia1121 <noviayya1121@gmail.com>
2024-07-06 09:22:31 +0700

    Perubahan dan Penambahan Fitur

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AccountController extends Controller
+{
+    public function registration()
+    {
+        return view('frontend.account.registration');
+    }
+
+    public function processRegistration(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name'=> 'required',
+            'email'=> 'required|email|unique:users,email',
+            'password'=> 'required|min:4|same:confirm_password',
+            'confirm_password'=> 'required',
+        ]);
+        if($validator->passes())
+        {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            session()->flash('success','You have registred Successfully');
+            return response()->json([
+                'status'=>true,
+                'errors'=>[]
+            ]);
+        }else{
+            return response()->json([
+                'status'=>false,
+                'errors'=>$validator->errors()
+            ]);
+        }
+    }
+
+    public function login()
+    {
+        return view('frontend.account.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'email'=> 'required|email',
+            'password'=> 'required',
+        ]);
+
+        if($validator->passes())
+        {
+            if(Auth::attempt(['email'=>$request->email, 'password'=>$request->password]))
+            {
+                return redirect()->route('account.profile');
+            }else{
+                return redirect()->route('account.login')->with('error', 'Either Email or Password is incorrect');
+            }
+        }else{
+            return redirect()->route('account.login')
+                ->withErrors($validator)
+                ->withInput($request->only('email'));
+        }
+    }
+
+    public function profile()
+    {
+        $id = Auth::user()->id;
+        $user = User::where('id',$id)->first();
+
+       return view('frontend.account.profile',[
+        'user'=>$user
+       ]);
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $id = Auth::user()->id;
+        $validator = Validator::make($request->all(),[
+            'name'=> 'required|min:4|max:20',
+            'email'=> 'required|email|unique:users,email,'.$id.'id',
+        ]);
+        if($validator->passes())
+        {
+            $user = User::find($id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->destination = $request->destination;
+            $user->mobile = $request->mobile;
+            $user->save();
+
+            session()->flash('success','Your profile Updated Successfully');
+            return response()->json([
+                'status'=>true,
+                'errors'=>[]
+            ]);
+        }else{
+            return response()->json([
+                'status'=>false,
+                'errors'=>$validator->errors()
+            ]);
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('account.login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+
+Route::group(['prefix'=>'account'], function () {
+    //Guest Route
+    Route::group(['middleware' => 'guest'], function () {
+        Route::get('/register', [AccountController::class, 'registration'])->name('account.registration');
+        Route::post('/process-register', [AccountController::class, 'processRegistration'])->name('account.processRegistration');
+        Route::get('/login', [AccountController::class, 'login'])->name('account.login');
+        Route::post('/authenticate', [AccountController::class, 'authenticate'])->name('account.authenticate');
+    });
+
+    //authenticated Route
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('/profile', [AccountController::class, 'profile'])->name('account.profile');
+        Route::put('/update-profile', [AccountController::class, 'updateProfile'])->name('account.updateProfile');
+        Route::get('/logout', [AccountController::class, 'logout'])->name('account.logout');
+    });
 });
